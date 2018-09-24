b96c8ca3704ccbaee8df89bf2268810509d04ed2
andrew@ameltek.co.uk <andrew@ameltek.co.uk>
2018-09-24 10:29:12 +0100

    Added customer object and tests

---
+++ BluePrism.Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace BluePrism.Domain
+{
+    public class Customer
+    {
+        public Customer(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ BluePrism.Tests/CustomerTests.cs
@@ -0,0 +1,24 @@
+﻿
+
+using BluePrism.Domain;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BluePrism.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        public void Customer_Can_Create_Customer_Test()
+        {
+            var customer = new Customer(1, "Andrew", "andrew@test.com");
+
+            customer
+                .Should()
+                .BeOfType(typeof(Customer));
+
+            customer.Name.Should().Be("Andrew");
+        }
+    }
+}
